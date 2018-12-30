ee5993c7a361816791c52d9920a6a2cb37067732
zihad07 <zihadnayem8@gmail.com>
2018-12-30 14:48:06 +0600

    1002 problem solved

---
+++ Beginner/1002_Area.py
@@ -0,0 +1,8 @@
+
+# Read radius from user
+Raddius = float(input())
+pi = 3.14159
+# Calculate Circle  Area
+Area = pi*Raddius*Raddius 
+
+print("A=%0.4f" %Area)
--- Beginner/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-
-# Read two value from user
-
-a, b = map(int, input().split())
-
-X = a + b
-
-print("X = "+str(X))
